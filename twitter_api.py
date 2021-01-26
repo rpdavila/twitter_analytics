460af2069221e777bbf9079631d517bf67b779e6
rpdavila <rafael.pietri@gmail.com>
2021-01-26 17:58:43 -0500

    added and switched db connection to store data function

--- twitter_api.py
+++ twitter_api.py
@@ -15,18 +15,6 @@ auth.set_access_token(access_token, secret_token)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 # connect to db
-try:
-    conn = mariadb.connect(
-        user=os.getenv("mariadb_user"),
-        password=os.getenv("mariadb_pass"),
-        host="192.168.1.5",
-        port=3306,
-        database="twitter"
-    )
-    cur = conn.cursor()
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
 
 
 # Function that grabs the name and country id of trending data
@@ -40,6 +28,15 @@ def trends_avail():
 
 def store_data(trend_name, trend_woeid):
     try:
+        conn = mariadb.connect(
+            user=os.getenv("mariadb_user"),
+            password=os.getenv("mariadb_pass"),
+            host="192.168.1.5",
+            port=3306,
+            database="twitter"
+        )
+
+        cur = conn.cursor()
         insert_query = 'INSERT INTO twitter.twitter_trends_available(name, woeid) VALUES (?,?)'
         cur.execute(insert_query, (trend_name, trend_woeid))
         conn.commit()
